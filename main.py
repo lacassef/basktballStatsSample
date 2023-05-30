43c76d364f65c4facfddc4167b9ff08db1904efa
Fidel Luis Inacio Lacasse <fluis.lacasse@gmail.com>
2023-05-30 10:41:55 +0200

    Development

--- main.py
+++ main.py
@@ -5,49 +5,73 @@ def match_processing(event):
     event_id = event["id"]
 
     # Match stats
-    statistics = api.get_match_statistics(event_id)
+    try:
+        statistics = api.get_match_statistics(event_id)
+    except Exception as ex:
+        # If no stats just return, skipping the match
+        print(f"HTTP error with the stats: {ex}")
+        return
 
     # We're just printing the stats, but you can do anything with the event and statistics like saving
     # to your database
     print(statistics)
-
     # Getting all other info
-    lineups = api.get_match_lineups(event_id)
-    form = api.get_pre_match_form(event_id)
-    match = api.get_match(event_id)
+    try:
+        lineups = api.get_match_lineups(event_id)
+    except Exception as ex:
+        # If no lineups just return, skipping the match
+        print(f"HTTP error with the lineups: {ex}")
+        return
+    form = None
+    try:
+        form = api.get_pre_match_form(event_id)
+    except Exception as ex:
+        # If no form you decide what action to do. It can happen in playoff matches
+        print(f"HTTP error with the form: {ex}")
+    try:
+        match = api.get_match(event_id)
+    except Exception as ex:
+        # Who knows what can happen to the match, if any error skip
+        print(f"HTTP error with the match: {ex}")
+        return
 
     # Let's get all needed variables from match details
 
     # Event start timestamp (in seconds)
-    startTimestamp = match["startTimestamp"]
+    startTimestamp = match["event"]["startTimestamp"]
 
     # Team names
-    homeTeamName, awayTeamName = match["homeTeam"]["name"], match["awayTeam"]["name"]
+    homeTeamName, awayTeamName = match["event"]["homeTeam"]["name"], match["event"]["awayTeam"]["name"]
 
     # Home team scores
-    homeTeamFinalScore, homeTeamP1Score, homeTeamP2Score, homeTeamP3Score, homeTeamP4Score = match["homeScore"][
-        "current"], match["homeScore"]["period1"], match["homeScore"]["period2"], match["homeScore"]["period3"], \
-    match["homeScore"]["period4"]
+    homeTeamFinalScore, homeTeamP1Score, homeTeamP2Score, homeTeamP3Score, homeTeamP4Score = \
+    match["event"]["homeScore"][
+        "current"], match["event"]["homeScore"]["period1"], match["event"]["homeScore"]["period2"], \
+    match["event"]["homeScore"]["period3"], \
+        match["event"]["homeScore"]["period4"]
 
     try:
-        homeTeamOvertimeScore = match["homeScore"]["overtime"]
+        homeTeamOvertimeScore = match["event"]["homeScore"]["overtime"]
     except:
         #     No overtime logic
         pass
 
     # Away team scores
-    awayTeamFinalScore, awayTeamP1Score, awayTeamP2Score, awayTeamP3Score, awayTeamP4Score = match["awayScore"][
-        "current"], match["awayScore"]["period1"], match["awayScore"]["period2"], match["awayScore"]["period3"], \
-    match["awayScore"]["period4"]
+    awayTeamFinalScore, awayTeamP1Score, awayTeamP2Score, awayTeamP3Score, awayTeamP4Score = \
+    match["event"]["awayScore"][
+        "current"], match["event"]["awayScore"]["period1"], match["event"]["awayScore"]["period2"], \
+    match["event"]["awayScore"]["period3"], \
+        match["event"]["awayScore"]["period4"]
 
     try:
-        awayTeamOvertimeScore = match["awayScore"]["overtime"]
+        awayTeamOvertimeScore = match["event"]["awayScore"]["overtime"]
     except:
         #     No overtime logic
         pass
 
     # Team standings
-    homeStandings, awayStandings = form["homeTeam"]["position"], form["awayTeam"]["position"]
+    if form is not None:
+        homeStandings, awayStandings = form["homeTeam"]["position"], form["awayTeam"]["position"]
 
     # Teams players These variables are arrays and the players details are in player(item)["player"] and the stats
     # are in player(item)["statistics"]
